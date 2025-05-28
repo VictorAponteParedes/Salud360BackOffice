731a5dbc6b7246963844fbfa6ab0b03d5ae74a4e
Victor Aponte <vaponte520@gmail.com>
2025-05-28 18:51:34 -0300

    corrige rutas de pacientes en App.tsx para incluir ListPatients y crear paciente

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardLayout from "./layouts/DashboardLayout";
 import Home from "./screens/Home";
-import Patients from "./screens/Patients";
+import Patients from "./screens/patients/Patients";
+import ListPatients from "./screens/patients/ListPatients";
 import Doctors from "./screens/Doctors";
 import "./index.css";
 
@@ -12,7 +13,8 @@ function App() {
       <DashboardLayout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/patients" element={<Patients />} />
+          <Route path="/patients/create" element={<Patients />} />
+          <Route path="/patients" element={<ListPatients />} />
           <Route path="/doctors" element={<Doctors />} />
         </Routes>
       </DashboardLayout>
