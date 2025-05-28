6a6382981c3e612cc02621de93ae4a1b71fc136a
Victor Aponte <vaponte520@gmail.com>
2025-05-28 16:56:01 -0300

    formatea el componente Home para mejorar la legibilidad del código

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <h1 className="text-2xl font-semibold">Bienvenido al Panel Salud360</h1>;
+  return (
+    <h1 className="text-2xl font-semibold">Bienvenido al Panel Salud360</h1>
+  );
 }
