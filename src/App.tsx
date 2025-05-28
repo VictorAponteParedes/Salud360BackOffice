da76914a56d5fd02ee45d55a0bdab4554f62cc1e
Victor Aponte <vaponte520@gmail.com>
2025-05-28 14:42:39 -0300

    Agrega: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Salud360Back Office
+      </h1>
+    </>
+  )
+}
+
+export default App
