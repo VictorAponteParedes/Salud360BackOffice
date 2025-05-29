ffd5bfabac5ebb10c984a0ed647e3cf8031b5f1f
Victor Aponte <vaponte520@gmail.com>
2025-05-28 22:24:27 -0300

    refactor: simplify App component by removing Router and DashboardLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-// src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DashboardLayout from "./layouts/DashboardLayout";
-import Home from "./screens/Home";
-import Patients from "./screens/patients/Patients";
-import ListPatients from "./screens/patients/ListPatients";
-import Doctors from "./screens/Doctors";
-import "./index.css";
+import Routes from './routes';
+import './index.css';
 
 function App() {
-  return (
-    <Router>
-      <DashboardLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/patients/create" element={<Patients />} />
-          <Route path="/patients" element={<ListPatients />} />
-          <Route path="/doctors" element={<Doctors />} />
-        </Routes>
-      </DashboardLayout>
-    </Router>
-  );
+  return <Routes />;
 }
 
 export default App;
