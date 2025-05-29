86e2ad11afb9cd653abfa582504be1de0b0ad549
Victor Aponte <vaponte520@gmail.com>
2025-05-28 22:24:51 -0300

    feat: implement routing with createBrowserRouter and AppRoutes

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
+import Home from '../screens/Home';
+import Patients from '../screens/patients/Patients';
+import PatientList from '../screens/patients/ListPatients';
+import ErrorPage from '../components/erroPage';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppRoutes />,
+        errorElement: <ErrorPage />,
+        children: [
+            { index: true, element: <Home /> },
+            { path: 'patients/create', element: <Patients /> },
+            { path: 'patients/list', element: <PatientList /> },
+        ],
+    },
+]);
+
+export default function Routes() {
+    return <RouterProvider router={router} />;
+}
