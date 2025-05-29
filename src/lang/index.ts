9275679e32db8acf37099b3f61e3b3d5a80fdc5d
Victor Aponte <vaponte520@gmail.com>
2025-05-29 10:13:02 -0300

    feat: agregar módulo de traducción para soportar el idioma español

---
+++ src/lang/index.ts
@@ -0,0 +1,31 @@
+import home from './Home.json';
+import patient from './Patient.json';
+
+// Definir el idioma actual, por ejemplo, 'es' para español
+const currentLanguage = 'es';
+
+
+const translations = {
+  es: {
+    // Traducciones para el idioma español
+    ...home,
+    ...patient
+  },
+
+};
+
+// Función para obtener la traducción
+const translate = (key) => {
+  const keys = key.split('.');
+  let result = translations[currentLanguage];
+
+  for (const k of keys) {
+    result = result[k];
+    if (result === undefined) return key; // Devuelve la clave si no encuentra la traducción
+  }
+
+  return result || key;
+};
+
+
+export { translate };
