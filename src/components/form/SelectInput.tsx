d0a5fb81e6e956cba30bb816e22695bc690f3efb
Victor Aponte <vaponte520@gmail.com>
2025-05-30 10:15:52 -0300

    feat: reemplazar SelectInput con MultiSelect para mejorar la selección de opciones

--- src/components/form/SelectInput.tsx
+++ src/components/form/SelectInput.tsx
@@ -1,22 +1,39 @@
-import { useFormContext } from 'react-hook-form';
-import type { SelectInputProps } from '../../types/input';
+// components/form/SelectInput.tsx
+import { Controller, useFormContext } from "react-hook-form";
+import { MultiSelect } from "primereact/multiselect";
+import type { SelectInputProps } from "../../types/input";
+import "primereact/resources/themes/lara-light-blue/theme.css"; // Asegúrate de importar el tema
+import "primereact/resources/primereact.min.css"; // Estilos base de primereact
 
-
-export const SelectInput = ({ name, label, options, placeholder }: SelectInputProps) => {
-  const { register, formState: { errors } } = useFormContext();
+export const SelectInput = ({
+  name,
+  label,
+  options,
+  placeholder,
+}: SelectInputProps) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
 
   return (
     <div className="mb-4">
       <label className="block mb-1 font-medium">{label}</label>
-      <select
-        {...register(name)}
-        className="w-full p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
-      >
-        <option value="">{placeholder ?? 'Seleccione una opción'}</option>
-        {options.map(({ label, value }) => (
-          <option key={value} value={value}>{label}</option>
-        ))}
-      </select>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <MultiSelect
+            value={field.value}
+            onChange={(e) => field.onChange(e.value)}
+            options={options}
+            optionLabel="label"
+            placeholder={placeholder ?? "Seleccione una opción"}
+            className="w-full"
+            display="chip"
+          />
+        )}
+      />
       {errors[name] && (
         <span className="text-red-500 text-sm">
           {(errors[name] as any).message}
