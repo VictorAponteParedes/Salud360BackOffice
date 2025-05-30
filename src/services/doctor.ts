d1692958de21881cd994826b0ca15d1cf562a5cb
Victor Aponte <vaponte520@gmail.com>
2025-05-29 23:29:17 -0300

    feat: implement DoctorService for managing doctor data and profile images

---
+++ src/services/doctor.ts
@@ -0,0 +1,62 @@
+import { API_BASE_URL } from "../constants";
+import axios from "axios";
+import { uploadApi } from "./patient";
+
+
+
+
+export class DoctorService {
+    async getDoctors() {
+        try {
+            const response = await axios.get(`${API_BASE_URL}/doctors`);
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching doctors:", error);
+            throw error;
+        }
+    }
+
+    async getDoctorById(doctorId: string) {
+        try {
+            const response = await axios.get(`${API_BASE_URL}/doctors/${doctorId}`);
+            return response.data;
+        } catch (error) {
+            console.error(`Error fetching doctor with ID ${doctorId}:`, error);
+            throw error;
+        }
+    }
+    async createDoctor(doctorData: any) {
+        try {
+            const response = await axios.post(`${API_BASE_URL}/doctors`, doctorData);
+            return response.data;
+        } catch (error) {
+            console.error("Error creating doctor:", error);
+            throw error;
+        }
+    }
+    async uploadImage(formDataUser: FormData) {
+        console.log('Datos imagen services: ', formDataUser)
+        try {
+            const response = await uploadApi.post('/upload', formDataUser, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            return response.data;
+        } catch (e: any) {
+            console.log('Error detallado:', e.response?.data || e.message);
+            throw e;
+        }
+    }
+    async getProfileImage(userId: any) {
+        try {
+            const response = await axios.get(`/doctors/${userId}/profile-image`);
+            console.log("Dat perfil imagen: ", response.data.url)
+            return response.data.url;
+        } catch (error) {
+            console.log('Error al obtener imagen de perfil', error);
+            return null;
+        }
+    }
+
+}
