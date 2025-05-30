735bf8ffd8d91eb251f7ab162967d700aeed593a
Victor Aponte <vaponte520@gmail.com>
2025-05-30 17:11:19 -0300

    feat: integrar el contexto de autenticación en las rutas de la aplicación

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,20 @@
 // src/routes/AppRoutes.tsx
 import { Outlet, Navigate } from 'react-router-dom';
 import DashboardLayout from '../layouts/DashboardLayout';
+import { useAuth } from "../context/AuthContext";
 
 export default function AppRoutes() {
-    const isAuthenticated = true
+  const { isAuthenticated } = useAuth();
+  console.log("AppRoutes - isAuthenticated:", isAuthenticated); // Debug
 
-    if (!isAuthenticated) {
-        return <Navigate to="/auth/login" replace />;
-    }
+  if (!isAuthenticated) {
+    console.log("No autenticado, redirigiendo a login...");
+    return <Navigate to="/auth/login" replace />;
+  }
 
-    return (
-        <DashboardLayout>
-            <Outlet />
-        </DashboardLayout>
-    );
+  return (
+    <DashboardLayout>
+      <Outlet />
+    </DashboardLayout>
+  );
 }
