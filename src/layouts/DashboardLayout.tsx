78bba479e6d5fa55752a383a0cd941d93397ae18
Victor Aponte <vaponte520@gmail.com>
2025-06-29 17:08:30 -0300

    feat(DashboardLayout): add link to create information card in the sidebar

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -110,6 +110,20 @@ export default function DashboardLayout({ children }: Props) {
                   <User className="w-5 h-5" />
                   {translate("layout.items.doctors")}
                 </Link>
+
+
+
+
+                <Link
+                  to={RoutesView.informationCardCreate}
+                  className="flex items-center gap-2 text-white px-3 py-2 rounded-md border-l-4 border-transparent hover:border-blue-600 hover:bg-blue-50 hover:text-blue-600 transition-all duration-200"
+                >
+                  <User className="w-5 h-5" />
+                  Crear informacion card
+                </Link>
+
+
+
                 <Link
                   to={RoutesView.listHospital}
                   className="flex items-center gap-2 text-white px-3 py-2 rounded-md border-l-4 border-transparent hover:border-blue-600 hover:bg-blue-50 hover:text-blue-600 transition-all duration-200"
