8913e596d2241391e3e87f287d36e21ea284164f
Victor Aponte <vaponte520@gmail.com>
2025-07-02 16:20:39 -0300

    fix(.gitignore): add .env files to ignore list refactor(index.ts): update API_BASE_URL to use environment variable

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 //Para levantar en la windonws
 export const LOCAL_IP = 'localhost' // Cambia esto por tu IP local si es necesario;
-export const API_BASE_URL = `http://${LOCAL_IP}:3000`
+// export const API_BASE_URL = `http://${LOCAL_IP}:3000`
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
