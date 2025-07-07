e6b85901a1d8fa57f16fdf6e2f2576d7b3e05bc3
Victor Aponte <vaponte520@gmail.com>
2025-07-06 22:18:58 -0300

    feat(informationCard): add InformationCard, InformationCardCreate, and InformationCardList components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,10 +18,16 @@ import AppointmentList from "../screens/appointment/AppointmentList";
 import AppointmentForm from "../screens/appointment/AppointmentCreate";
 import { RoutesView } from "./route";
 import VerifyCode from "../screens/auth/VerifyCode";
+
+
+//Information Card
+import InformationCardCreate from "../screens/informationCard/InformationCardCreate";
+import InformationCardList from "../screens/informationCard/InformationCardList";
+
+
+//hospitals
 import HospitalList from "../screens/hospitals/HospitalLista";
 import HospitalDetails from "../screens/hospitals/HospitalDetails";
-import InformationCardCreate from "../screens/information-card/InformationCardCreate";
-import InformationCardList from "../screens/information-card/InformationCardList";
 import HospitalCreate from "../screens/hospitals/HospitalCreate";
 
 
