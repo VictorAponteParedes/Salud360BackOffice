db5a7eca99a71a5e283196af94555402806eb552
Victor Aponte <vaponte520@gmail.com>
2025-07-11 17:29:03 -0300

    Agrega: ajuste de constante entorno

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 //Para levantar en la windonws
 export const LOCAL_IP = 'localhost' // Cambia esto por tu IP local si es necesario;
-export const API_BASE_URL = `http://${LOCAL_IP}:3000`
+// export const API_BASE_URL = `http://${LOCAL_IP}:3000`
 // En tu archivo de configuración (ej. src/config.js)
-// export const API_BASE_URL = import.meta.env.VITE_API_URL
+export const API_BASE_URL = import.meta.env.VITE_API_URL
